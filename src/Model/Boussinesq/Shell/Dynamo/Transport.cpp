b34b86c2bcc91934098077268710075f1fce6589
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2023-09-19 09:53:47 +0200

    remove math direct dependecy (#21)

--- src/Model/Boussinesq/Shell/Dynamo/Transport.cpp
+++ src/Model/Boussinesq/Shell/Dynamo/Transport.cpp
@@ -19,7 +19,6 @@
 // Project includes
 //
 #include "QuICC/Transform/Path/Scalar.hpp"
-#include "QuICC/Math/Constants.hpp"
 #include "QuICC/PhysicalNames/Temperature.hpp"
 #include "QuICC/PhysicalNames/Velocity.hpp"
 #include "QuICC/SolveTiming/Prognostic.hpp"
